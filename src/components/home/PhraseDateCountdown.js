67838ae7344bf6577ddf3b0aab2df709defd0745
umiphos <umiphos@gmail.com>
2024-11-05 02:24:56 -0600

    ADD: added style and information for section 2

--- src/components/home/PhraseDateCountdown.js
+++ src/components/home/PhraseDateCountdown.js
@@ -2,20 +2,12 @@ import React from 'react';
 
 function PhraseDateCountdownPage() {
   return (
-    <section>
+    <section className='phrase-date-countdown-section'>
       <div>
-        <h3>Nuestros Padres</h3>
-        <ul>
-          <li> CARLOS AGUILAR VARGAS</li>
-          <li>MA. LOURDES AIDA CERVANTES GARCIA</li>
-
-          <li>a</li>
-          <li>a</li>
-        </ul>
-        <h3>Nuestros Padrinos</h3>
-
+        <p>
+          PREFIERO VIVIR UNA VIDA CONTIGO, QUE PASAR EL RESTO DE LAS EDADES SIN TI
+        </p>
       </div>
-
     </section>
   );
 }
--- src/components/home/VIPGuests.js
+++ src/components/home/VIPGuests.js
@@ -4,15 +4,22 @@ function VIPGuests() {
   return (
     <section>
       <div>
-        <h3>Nuestros Padres</h3>
+      <h3>Nuestros Padres</h3>
         <ul>
-          <li> CARLOS AGUILAR VARGAS</li>
-          <li>MA. LOURDES AIDA CERVANTES GARCIA</li>
-
-          <li>a</li>
-          <li>a</li>
+          CARLOS AGUILAR VARGAS<br/>
+          MA. LOURDES AIDA CERVANTES GARCIA<br/>
+          ------------------------------------------------------------<br/>
+          MA. MERICIA GALVEZ MONRROY<br/>
+          JAVIER VICENTE FRUTO(+)<br/>
         </ul>
         <h3>Nuestros Padrinos</h3>
+        <ul>
+          JOSE LUIS CERVANTES GARCIA<br/>
+          LETICIA AURORA CERVANTES GARCIA<br/>
+          ------------------------------------------------------------<br/>
+          LEONEL GALVEZ MONRROY<br/>
+          MARIA FELIZ RIVERA JUAREZ<br/>
+        </ul>
 
       </div>
 
