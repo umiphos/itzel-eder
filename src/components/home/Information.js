892b21e99f6ef88a6101eb7689e28e9e9c327de3
umiphos <umiphos@gmail.com>
2024-11-07 00:44:34 -0600

    FIX: fixed information section

--- src/components/home/Information.js
+++ src/components/home/Information.js
@@ -5,20 +5,31 @@ function Information() {
 <section class="seccion">
   <div class="contenedor-columnas">
     <div class="columna">
-      <h3>Ceremonia</h3>
+      <div class="header-container">
+        <img src='/icons/icono_ceremonia.png' alt="Ceremonia Icon" class="icon"/>
+        <h3>Ceremonia 5:00 PM</h3>
+      </div>
       <ul>
-      <li>Do cillum et occaecat quis. Officia Lorem et consectetur consequat id ea anim nisi sunt sunt non nulla aute adipisicing. Duis nisi cupidatat veniam cupidatat culpa. Ipsum culpa ad nostrud proident adipisicing nostrud do amet quis sit exercitation ut sunt nulla.</li>
+        <li>Parroquia del espiritu santo</li>
+        <li>Avenida Felipe Sevilla del Rio S/N</li>
+        <li><a href="https://www.google.com/maps/place/Parroquia+del+Espíritu+Santo" target="_blank">Maps</a></li>
       </ul>
     </div>
 
     <div class="columna">
-      <h3>Recepción</h3>
+      <div class="header-container">
+        <img src='/icons/icon_reception.png' alt="Recepción Icon" class="icon"/>
+        <h3>Recepción 7:00 PM</h3>
+      </div>
       <ul>
-        <li>Do cillum et occaecat quis. Officia Lorem et consectetur consequat id ea anim nisi sunt sunt non nulla aute adipisicing. Duis nisi cupidatat veniam cupidatat culpa. Ipsum culpa ad nostrud proident adipisicing nostrud do amet quis sit exercitation ut sunt nulla.</li>
+        <li>Hacienda Pastores, 4.5KM</li>
+        <li>Hacia el norte de la Hacienda del Carmen, antes de llegar al Chivato, Villa de Alvarez Colima</li>
+        <li><a href="https://www.google.com/maps/place/Hacienda+Pastores" target="_blank">Maps</a></li>
       </ul>
     </div>
   </div>
 </section>
+
 
   );
 }
