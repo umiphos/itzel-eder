a6c76e3a81563adbacb99b8a466ddc4609cd0f18
umiphos <umiphos@gmail.com>
2024-11-09 14:54:27 -0600

    fixup! ADD: music

--- src/components/home/Music.js
+++ src/components/home/Music.js
@@ -3,6 +3,7 @@ import './MusicPlayer.css';
 
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [hasUserApproved, setHasUserApproved] = useState(false); // Estado para saber si el usuario ha aprobado
   const audioRef = useRef(null);
 
   const togglePlayPause = () => {
@@ -14,26 +15,48 @@ const MusicPlayer = () => {
     setIsPlaying(!isPlaying);
   };
 
+  const handleApproval = () => {
+    setHasUserApproved(true);
+    audioRef.current.play(); // Empieza a reproducir la canción
+    setIsPlaying(true);
+  };
+
   useEffect(() => {
-    // Opcional: Autoplay cuando el componente se monta
-     audioRef.current.play();
-     setIsPlaying(true);
+    // No reproducir automáticamente hasta que el usuario haya aprobado
+    if (hasUserApproved) {
+      audioRef.current.play();
+    }
 
     return () => {
       audioRef.current.pause();
     };
-  }, []);
+  }, [hasUserApproved]);
 
   return (
-    <div className="music-player">
-      <audio ref={audioRef} src="/files/song.mp3" loop autoPlay/>
-      <button className="play-pause-button" onClick={togglePlayPause}>
-        <img
-          src={"/icons/music_icon.gif"}
-          alt="Music Icon"
-          className={`music-icon ${isPlaying ? 'playing' : 'paused'}`}
-        />
-      </button>
+    <div className="music-player-container">
+      <audio ref={audioRef} src="/files/song.mp3" loop />
+
+      {!hasUserApproved && (
+        <div className="welcome-overlay">
+          <div className="welcome-message">
+            <h1>¡Bienvenido!</h1>
+            <p>Haz clic en el botón para empezar a escuchar música.</p>
+            <button onClick={handleApproval}>Reproducir Música</button>
+          </div>
+        </div>
+      )}
+
+      {hasUserApproved && (
+        <div className="music-player">
+          <button className="play-pause-button" onClick={togglePlayPause}>
+            <img
+              src={"/icons/music_icon.gif"}
+              alt="Music Icon"
+              className={`music-icon ${isPlaying ? 'playing' : 'paused'}`}
+            />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
